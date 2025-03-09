c71b2989b5a0ee4852ba37ad35ed4acf13151f32
Paul Collins <paul@digitalfish.com>
2025-03-08 17:59:26 -0800

    Initial Commit

---
+++ Utter/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Utter
+//
+//  Created by Paul Collins on 3/8/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Utter/UtterApp.swift
@@ -0,0 +1,17 @@
+//
+//  UtterApp.swift
+//  Utter
+//
+//  Created by Paul Collins on 3/8/25.
+//
+
+import SwiftUI
+
+@main
+struct UtterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
